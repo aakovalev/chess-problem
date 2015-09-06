8be41bca7f556c46488e873c383c8cfb8f8bdc46
Alexey Kovalev <Alexey.Kovalev@emc.com>
2015-09-06 11:56:34 +0300

    implements some de-generate and trivial test cases

--- src/test/java/ChessProblemTest.java
+++ src/test/java/ChessProblemTest.java
@@ -1,12 +1,39 @@
 import org.junit.Test;
 
-/**
- * @author kovala4
- * @since 9/6/2015
- */
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class ChessProblemTest {
     @Test
     public void nothing() throws Exception {
+        assertThat(numberOfUniqueConfigurations(1, 1, null), is(0));
+        assertThat(numberOfUniqueConfigurations(1, 1, ""), is(0));
+        assertThat(numberOfUniqueConfigurations(1, 1, "1K"), is(1));
+    }
 
+    @Test (expected = IllegalArgumentException.class)
+    public void dimensionOfTheBoardShouldPositiveNumber() throws Exception {
+        numberOfUniqueConfigurations(0, -1, "");
+    }
+
+    @Test
+    public void whenNumberOfFiguresExceedsNumberOfCellsOfLayoutReturnsZero()
+            throws Exception {
+        assertThat(numberOfUniqueConfigurations(1, 1, "2K"), is(0));
+    }
+
+    private int numberOfUniqueConfigurations(int m, int n, String figureSpec) {
+        if (notPositive(m) || notPositive(n)) {
+            throw new IllegalArgumentException(
+                    "Dimension of layout can NOT be a negative number");
+        }
+        if (figureSpec == null || figureSpec.length() == 0) {
+            return 0;
+        }
+        return 1;
+    }
+
+    private boolean notPositive(int value) {
+        return value <= 0;
     }
 }
